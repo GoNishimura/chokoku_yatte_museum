d643831d36f253e2b7767f2a75527ff8399769c2
GoNishimura <go.nishim@gmail.com>
2020-05-13 13:30:37 +0900

    :beginner: test commit on web image access

---
+++ similarity.js
@@ -0,0 +1,16 @@
+var pose1ImageElement = document.getElementById('pose1');
+var pose2ImageElement = document.getElementById('pose2');
+
+// For more detailed Posenet setup, please refer its own document.
+// Load Posenet model
+posenet.load().then(function(net) {
+  // Estimate the two poses
+  return Promise.all([
+    net.estimateSinglePose(pose1ImageElement),
+    net.estimateSinglePose(pose2ImageElement)
+  ])
+}).then(function(poses){
+  // Calculate the weighted distance between the two poses
+  var weightedDistance = pns.poseSimilarity(poses[0], poses[1], {strategy: 'cosineSimilarity'});
+  console.log(weightedDistance)
+})
